d0b140d2bb91bbd43545d56695c3bac6dce42b1a
Grigorich <oskinnick@gmail.com>
2023-07-06 17:38:27 +0300

    Initial commit

---
+++ src/main/java/org/mykola/spilcaSmallProject1/SpilcaSmallProject1Application.java
@@ -0,0 +1,13 @@
+package org.mykola.spilcaSmallProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpilcaSmallProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpilcaSmallProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/mykola/spilcaSmallProject1/SpilcaSmallProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mykola.spilcaSmallProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpilcaSmallProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
