cacfaa2bd2a80812e8bc248f9e758bac15f2786d
Grigorich <oskinnick@gmail.com>
2023-07-10 16:25:15 +0300

    ProjectConfig.java completed

--- src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
+++ src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
@@ -27,6 +28,15 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+        http.authorizeRequests()
+                .anyRequest().authenticated();
     }
 
 }
