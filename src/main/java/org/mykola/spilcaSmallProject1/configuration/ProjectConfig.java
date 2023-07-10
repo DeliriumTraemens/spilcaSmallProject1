02ff905251802cea9cf98b10ce9ea95cdcac6c44
Grigorich <oskinnick@gmail.com>
2023-07-10 15:55:57 +0300

    ProjectConfig.java developing

--- src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
+++ src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
@@ -1,12 +1,17 @@
 package org.mykola.spilcaSmallProject1.configuration;
 
+import org.mykola.spilcaSmallProject1.service.AuthenticationProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthenticationProviderService authenticationProvider;
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
