5c1b2d1451dc29a902a78f298f488f402336579f
Grigorich <oskinnick@gmail.com>
2023-07-10 16:24:10 +0300

    ProjectConfig.java filled

--- src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
+++ src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
@@ -4,6 +4,7 @@ import org.mykola.spilcaSmallProject1.service.AuthenticationProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
@@ -12,14 +13,20 @@ import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationProviderService authenticationProvider;
+
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
-    public SCryptPasswordEncoder sCryptPasswordEncoder(){
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
         return new SCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
+    }
 
 }
