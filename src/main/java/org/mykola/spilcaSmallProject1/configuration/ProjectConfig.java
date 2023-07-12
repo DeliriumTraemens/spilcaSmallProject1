e2bb3fe180076b432fcabfd72eddc5bf313b1caf
Grigorich <oskinnick@gmail.com>
2023-07-12 20:24:24 +0300

    ProjectConfig.java added configure() httpBasic and authorizeRequests

--- src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
+++ src/main/java/org/mykola/spilcaSmallProject1/configuration/ProjectConfig.java
@@ -2,6 +2,8 @@ package org.mykola.spilcaSmallProject1.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,13 +11,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-//    1 UserDetails wired by associated *Service
+    //    1 UserDetails wired by associated *Service
 //    В данном кейсе возвращает бин InMemoryUserDetailsService с ЗАРЯЖЕННЫМИ ЮЗЕРАМИ
 //    По сути это локальная ДБ в памяти
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 //      Define Users by UserDetails User
         var user1 = User.withUsername("john")
@@ -32,9 +34,18 @@ public class ProjectConfig {
 
         return manager;
     }
-//  2 PasswordEncoder wired
+
+    //  2 PasswordEncoder wired
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    // После зарядки бинов пишем конфиги с подробностями
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest().permitAll();
     }
 }
