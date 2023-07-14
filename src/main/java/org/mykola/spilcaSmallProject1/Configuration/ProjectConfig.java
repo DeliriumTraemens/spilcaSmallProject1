ead76091076478e2a767a4e502ba1226c7eb3b67
Grigorich <oskinnick@gmail.com>
2023-07-14 16:41:02 +0300

    ProjectConfig.java added with UserDetails and PassEncoder beans

---
+++ src/main/java/org/mykola/spilcaSmallProject1/Configuration/ProjectConfig.java
@@ -0,0 +1,37 @@
+package org.mykola.spilcaSmallProject1.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+        var user1 = User.withUsername("John")
+                .password("12345")
+                .roles("admin")
+                .build();
+        var user2 = User.withUsername("Jane")
+                .password("12345")
+                .roles("manager")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
